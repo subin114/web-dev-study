4c7efa3460fa03525da0db229548cb78f6fb1d4e
subin114 <bin7851@naver.com>
2024-05-29 00:57:09 +0900

    TanStack Query 기타 옵션 사용

--- react-query-practice/src/page/ReactQueryPage.jsx
+++ react-query-practice/src/page/ReactQueryPage.jsx
@@ -2,14 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const ReactQueryPage = () => {
-  const fetchPost = () => {
-    return axios.get("http://localhost:5000/posts");
+  const fetchPost = (queryData) => {
+    const id = queryData.queryKey[1];
+    return axios.get(`http://localhost:5000/posts/${id}`);
   };
 
-  const { isLoading, data, isError, error } = useQuery({
-    queryKey: ["posts"],
+  const { isLoading, data, isError, error, refetch } = useQuery({
+    queryKey: ["posts", 1], // 여러 값 작성 가능. 유니크하기만 하면 됨
     queryFn: fetchPost,
     retry: 1,
+    // staleTime: 60000, // 1분간 api 호출 금지
+    // gcTime: 10000, // 10초만 캐시가 유지. stale < gctime
+    // enabled: false, // 버튼 클릭할 때만 API 호출
     select: (data) => {
       return data.data;
     },
@@ -26,9 +30,10 @@ const ReactQueryPage = () => {
   }
   return (
     <div>
-      {data.map((item, idx) => (
+      {/* {data?.map((item, idx) => (
         <div key={idx}>{item.title}</div>
-      ))}
+      ))} */}
+      <button onClick={refetch}>post 리스트 다시 들고오기</button>
     </div>
   );
 };
